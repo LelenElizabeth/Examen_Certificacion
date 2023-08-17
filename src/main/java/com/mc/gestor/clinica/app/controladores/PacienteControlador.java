51ce00ace85f66edccefd20a6a886d255ab85362
Lelen <ecp.marlene@hotmail.com>
2023-08-17 04:14:15 -0400

    Se agrega Historial clinico y boton Editar

--- src/main/java/com/mc/gestor/clinica/app/controladores/PacienteControlador.java
+++ src/main/java/com/mc/gestor/clinica/app/controladores/PacienteControlador.java
@@ -1,5 +1,6 @@
 package com.mc.gestor.clinica.app.controladores;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mc.gestor.clinica.app.entidades.HistoriaClinica;
 import com.mc.gestor.clinica.app.entidades.Paciente;
+import com.mc.gestor.clinica.app.servicios.HistoriaClinicaServicio;
 import com.mc.gestor.clinica.app.servicios.PacienteServicio;
 
 @Controller
@@ -17,6 +20,9 @@ public class PacienteControlador {
 	@Autowired
 	private PacienteServicio pacienteServicio;
 	
+	@Autowired
+	private HistoriaClinicaServicio historiaClinicaServicio;
+	
 	@GetMapping("/buscar")
     public String buscarPacientesPorNombre(@RequestParam("parametro") String parametro, Model model) {
         List<Paciente> pacientes = pacienteServicio.buscarPorIdONombre(parametro);
@@ -24,4 +30,24 @@ public class PacienteControlador {
         model.addAttribute("parametroIngresado", parametro);
         return "inicio";
     }
+	
+	@GetMapping("/historia")
+	public String historiaClinica(@RequestParam(name = "parametro", required = false) String parametro,
+									@RequestParam(name = "pacienteId", required = false) Integer pacienteId, Model model) {
+		List<Paciente> pacientes = new ArrayList<>();
+		List<HistoriaClinica> historiasClinicas = new ArrayList<>();
+
+		if (parametro != null) {
+			pacientes = pacienteServicio.buscarPorIdONombre(parametro);
+		}
+
+		if (pacienteId != null) {
+			historiasClinicas = historiaClinicaServicio.buscarPorPacienteId(pacienteId);
+		}
+
+		model.addAttribute("pacientes", pacientes);
+		model.addAttribute("historiasClinicas", historiasClinicas);
+		model.addAttribute("parametroIngresado", parametro);
+		return "inicio";
+	}
 }
